07a35c5b067f5b9ede9d7ce9938ab5530642cb90
vinmugambi <vinmugambi17@gmail.com>
2024-12-01 23:49:56 +0300

    fix: throw http error if downstream returns 4xx error

--- app/services/api.server.ts
+++ app/services/api.server.ts
@@ -12,7 +12,12 @@ export async function getData<T>(endpoint: string): Promise<T> {
       if (response.status >= 500 && response.status < 600) {
         throw new Error("Request to downstream server failed");
       } else if (response.status >= 400 && response.status < 500) {
-        throw new Error(errorData?.error || "An unknown client error occurred");
+        throw new Response(
+          errorData?.error || "An unknown client error occurred",
+          {
+            status: response.status,
+          }
+        );
       }
     }
 
