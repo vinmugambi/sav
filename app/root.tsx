2166e13ab4911616a49270e38407f9312c1f7b8a
vinmugambi <vinmugambi17@gmail.com>
2024-12-02 00:01:27 +0300

    fix(ci): reconfigure vercel to deploy app to functions instead of edge

--- app/root.tsx
+++ app/root.tsx
@@ -26,8 +26,6 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export const config = { runtime: "edge" };
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
